7b6a13c81aa87e9eedcbad87ebc7c591b8b75505
Ian Henry <ian@fogcreek.com>
2014-05-15 18:07:16 -0400

    got a basic trello board rendering thingy set up

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject trello-time-travel "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [om "0.6.2"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :cljsbuild {:repl-listen-port 9001
+              :builds [{:id "dev"
+                        :source-paths ["src"]
+                        :compiler {:output-to "main.js"
+                                   :output-dir "out"
+                                   :optimizations :none
+                                   :source-map true}}]})
+
+
